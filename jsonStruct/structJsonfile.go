fda4615acf375973a7c6f37fbed44587312db0e7
Mukesh Khod <mukesh.khod@razorpay.com>
2019-07-16 15:34:23 +0530

    readme updated, unrelated files removed

--- jsonStruct/structJsonfile.go
+++
@@ -1,9 +0,0 @@
-package jsonStruct
-
-type Users struct {
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	Phone   string `json:"phone"`
-	Detail1 string `json:"detail1"`
-	Detail2 string `json:"detail2"`
-}
